7610de99a1b73945cf33c07c1b297ceb69896e7c
brobertson <brobertson@mta.ca>
2010-06-13 14:42:49 -0300

    now working; use colons throughout urn

--- Client/Moffatt/moffatt.js
+++ Client/Moffatt/moffatt.js
@@ -27,79 +27,82 @@
 var endpoint = "http://heml.mta.ca/sesame/openrdf-sesame/repositories/labels";
 
 function onHemlFailure(reply) {
-    alert("The Heml SPARQL query failed. Perhaps you do not have an Internet connection:\n" + reply);
+    alert("The Heml SPARQL query failed. Perhaps you do not have an Internet connection:n" + reply);
     // do something more interesting, like putting an x through the map. or making
     // a popup
 }
 
 function labelFromJson(json, lang) {
-	for (row in json.results.bindings) {
-		//console.warn(json.results.bindings[row]);
-		if (json.results.bindings[row].label["xml:lang"] == lang) return json.results.bindings[row].label["value"];
-	}
-	return null;
+    for (row in json.results.bindings) {
+        //console.warn(json.results.bindings[row]);
+        if (json.results.bindings[row].label["xml:lang"] == lang) return json.results.bindings[row].label["value"];
+    }
+    return null;
 }
-	
-var successfulCallbackModelForTitles = function(className, htmlNode) {
+
+var successfulCallbackModelForTitles = function(className, htmlNode, langPrefs) {
     //use '' for root
     this.htmlNode = htmlNode;
+    this.langPrefs = langPrefs;
     var me = this;
     this.makeRefTitles = function(json) {
-		var numberOfEntries = json.results.bindings.length;
-		if (numberOfEntries == 0) {
-			alert("No labels have been provided for this entry.");
-		} else {
-			htmlNode.appendChild(document.createTextNode(labelFromJson(json, 'it')));
-		}
-	}
+        var numberOfEntries = json.results.bindings.length;
+        if (numberOfEntries == 0) {
+            alert("No labels have been provided for this entry.");
+        } else {
+            htmlNode.appendChild(document.createTextNode(labelFromJson(json, 'it')));
+        }
+    }
 }
 
- $(document).ready(function () {
-				   console.warn($("[data-ctsurn]"));
-	  $("[data-ctsurn]").each(function(index) {
-		urn = $(this).attr("data-ctsurn");
-	//	myregexp =  /(\w+:cts:[A-Z]{4,}:([A-Z]{3}\d{4})\.([A-Z]{3}\d{3}))/i
-			myregexp =  /(\w+:cts:[A-Z]{4,}:([A-Z]{3}\d{4})\.([A-Z]{3}\d{3}))(.*)/i
-		mymatch = myregexp.exec(urn);
-							  console.warn(mymatch);
-		if(mymatch!=null){
-				              bookLine1 = mymatch[4];
-							console.warn("bookLine1: " + bookLine1);
-							  bookline2 = bookLine1.replace(/:/g,'.').substring(1);
-							  console.warn("bookline2: " + bookline2);
-							  var x = document.createElement('span');
-							  x.className = 'ctsurn_author';
-							  var y = document.createElement('span');
-							  y.className = 'ctsurn_title';
+$(document).ready(function() {
+    console.warn($("[data-ctsurn]"));
+    $("[data-ctsurn]").each(function(index) {
+        urn = $(this).attr("data-ctsurn");
+        console.warn(urn);
+        //  myregexp =  /(w+:cts:[A-Z]{4,}:([A-Z]{3}d{4}).([A-Z]{3}d{3}))/i
+         myregexp = /cts:greekLit:([A-Z]*\d*):([A-Z]*\d*)(.*)/i
+       // myregexp = /(w+:cts:[A-Z]{4,}:([A-Z]{3}\d{4}).([A-Z]{3}\d{3}))/i
+        mymatch = myregexp.exec(urn);
+        console.warn(mymatch);
+        if (mymatch != null) {
+            bookLine1 = mymatch[3];
+            console.warn("bookLine1: " + bookLine1);
+            bookline2 = bookLine1.replace(/:/g, '.').substring(1);
+            console.warn("bookline2: " + bookline2);
+            var x = document.createElement('span');
+            x.className = 'ctsurn_author';
+            var y = document.createElement('span');
+            y.className = 'ctsurn_title';
 
-							  $(this).val('');
-							  $(this).append(x);
-							  $(this).append(' ');
-							$(this).append(y);
-							  if (bookline2 != '') {
-							  var z = document.createElement('span');
-							  z.className = 'ctsurn_ref';
-								z.appendChild(document.createTextNode(bookline2));
-							  
-							  $(this).append(' ');
-							  $(this).append(z);
-							  }
-							  
-		var author ="<http://heml.mta.ca/text/urn/"+mymatch[2]+">"; // author URL
-		var work = "<http://heml.mta.ca/text/urn/"+mymatch[2]+"/"+mymatch[3]+">"; // work URL
-		var queryString2 = "select ?label where {"+author+" rdfs:label ?label. }"
-		myTry = new successfulCallbackModelForTitles("author", x);
-		hq2 = new Heml.SparqlQuery(endpoint, queryString2, onHemlFailure, myTry.makeRefTitles);
-						
-		hq2.performQuery();
-        myTry = new successfulCallbackModelForTitles("title", y);
-		var queryString = "select ?label where {"+work+" rdfs:label ?label. }"
-		hq = new Heml.SparqlQuery(endpoint, queryString, onHemlFailure, myTry.makeRefTitles);
-						hq.performQuery();
-	}	
-	});
-	});
-	
-	
+            $(this).val('');
+            $(this).append(x);
+            $(this).append(' ');
+            $(this).append(y);
+            if (bookline2 != '') {
+                var z = document.createElement('span');
+                z.className = 'ctsurn_ref';
+                z.appendChild(document.createTextNode(bookline2));
+                $(this).append(' ');
+                $(this).append(z);
+            }
+            var author = "<http://heml.mta.ca/text/urn/" + mymatch[1] + ">";
+            // author URL
+            var work = "<http://heml.mta.ca/text/urn/" + mymatch[1] + "/" + mymatch[2] + ">";
+            // work URL
+            var queryString2 = "select ?label where {" + author + " rdfs:label ?label. }"
+            var langPrefs = getPreferencesFromCookie();
+            myTry = new successfulCallbackModelForTitles("author", x, langPrefs);
+            hq2 = new Heml.SparqlQuery(endpoint, queryString2, onHemlFailure, myTry.makeRefTitles);
+            hq2.performQuery();
+            myTry = new successfulCallbackModelForTitles("title", y, langPrefs);
+            var queryString = "select ?label where {" + work + " rdfs:label ?label. }"
+            hq = new Heml.SparqlQuery(endpoint, queryString, onHemlFailure, myTry.makeRefTitles);
+            hq.performQuery();
+        }
+    });
+});
+
+
 
 
